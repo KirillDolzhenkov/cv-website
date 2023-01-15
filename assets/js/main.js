5a738bab3ec840e9a8e9784d94e04ac91819ea7f
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2023-01-15 22:43:28 +0300

    created skills area

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//navbar
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navCLose = document.getElementById('nav-close');
@@ -22,3 +23,23 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+//skills
+const skillsContent = document.getElementsByClassName("skills-content");
+const skillsHeader = document.querySelectorAll(".skills-header");
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = "skills-content skills-close"
+    }
+
+    if (itemClass === 'skills-content skills-close') {
+        this.parentNode.classList = "skills-content skills-open"
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+})
