71f1951d83c9b6d727f3cd5910cbb507a0fce7c4
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2023-01-20 17:31:53 +0300

    created media queries for different screen types

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,26 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills);
 })
+
+//dark theme
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sum';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+/*const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'moon' : 'sun';*///!
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    /*themeButton.classList[selectedIcon === 'moon' ? 'sun' : 'remove'](iconTheme);*///!
+}
+
+themeButton.addEventListener('click',() => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    /*localStorage.setItem('selected-icon', getCurrentTheme());*/
+});
